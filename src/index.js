bfc636b91fad9afb3aaeffa2eab26cd996a8aabf
TimusJon <musjohn.98@gmail.com>
2019-07-05 19:37:50 +0500

    1st_task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,28 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let length = preferences.length;
+  let count = 0;
+
+  for (let i = 0; i < length; i++) {
+    let next = preferences[i] - 1;
+    if (next == i) {
+      continue;
+    }
+
+    let next_Next = preferences[next] - 1;
+    if (next == next_Next) {
+      continue;
+    }
+
+    let next_Next_Next = preferences[next_Next] - 1;
+    if (next_Next_Next == next_Next) {
+      continue;
+    }
+
+    if (next_Next_Next == i) {
+      count++;
+    }
+  }
+  return count / 3;
 };
